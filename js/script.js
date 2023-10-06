175a034914cab99412e2e34a27f0809bd41d4ebf
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-06 14:21:09 +0200

    [add]:get for Api generated email

--- js/script.js
+++ js/script.js
@@ -6,10 +6,23 @@ createApp({
   data(){
     return{
 
-      title : 'List Emails:'
-
+      title : 'List Emails:',
+      generatedEmail : []
     }
+  },
+  methods:{
+    getApi(){
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then((myResponse)=>{
+        this.generatedEmail = myResponse.data.response
+      })
+      .catch((error)=>{
+        console.log(error);
+      })
+    }
+  },
+
+  mounted(){
+    this.getApi();
   }
-
-
 }).mount('#app')
