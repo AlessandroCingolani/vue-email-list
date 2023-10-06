9837638768cfb8be6ceeefb29e2c314ee9cec021
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-06 14:33:51 +0200

    [add]:function loop for get 10 random mails

--- js/script.js
+++ js/script.js
@@ -14,15 +14,22 @@ createApp({
     getApi(){
       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
       .then((myResponse)=>{
-        this.generatedEmail = myResponse.data.response
+        this.generatedEmail.push(myResponse.data.response)
       })
       .catch((error)=>{
         console.log(error);
       })
+    },
+    generateEmail(n){
+      for(let i = 0;i <= n;i++){
+        this.getApi();
+      }
     }
   },
 
+
   mounted(){
     this.getApi();
+    this.generateEmail(10)
   }
 }).mount('#app')
